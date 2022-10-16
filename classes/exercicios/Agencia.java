95c27574d987208aded331b43f52dae02f562d1a
Marcos Antonio <33884828+Marcos-Antonio1@users.noreply.github.com>
2022-10-16 19:44:44 -0300

    Create Agencia.java

---
+++ classes/exercicios/Agencia.java
@@ -0,0 +1,15 @@
+public class Agencia {
+    
+    public Agencia(int numero){
+        this.numero = numero;
+    }
+
+    int numero;
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+}
